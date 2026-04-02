9e2e533b9b66ba828866e644a00a7cbdef8163b6
Nastya <ael24@tpu.ru>
2026-04-02 17:58:18 +0000

    Last ver Counter

--- src/Counter.cpp
+++ src/Counter.cpp
@@ -1,41 +1,102 @@
 #include "ICounter.h"
 #include <fstream>
 
-// Конструктор счетчика
 LineCounter::LineCounter(std::shared_ptr<ILogger> log) : logger(log) {}
 
-// Логика подсчета (Правило: Логика отдельно от UI)
 CountResult LineCounter::count(const std::string& path) {
     CountResult res;
     std::ifstream file(path);
     std::string line;
-    bool inComment = false;
+    bool inBlockComment = false;
 
-    if (!file.is_open()) return res;
+    if (!file.is_open()) {
+        if (logger) logger->log("Error: File not found - " + path);
+        return res;
+    }
+
+    if (logger) logger->log("Processing: " + path);
 
     while (std::getline(file, line)) {
         res.physical++;
+        
+        // Проверяем, есть ли в строке код (не только комментарии)
+        bool hasCode = false;
         bool isComment = false;
-        bool isEmpty = line.find_first_not_of(" \t\r\n") == std::string::npos;
-
-        if (inComment) {
-            isComment = true;
-            if (line.find("*/") != std::string::npos) inComment = false;
+        bool wasInBlock = inBlockComment;
+        
+        // Простая проверка: ищем код до комментариев
+        size_t pos = 0;
+        std::string trimmedLine = line;
+        
+        // Удаляем ведущие пробелы
+        size_t start = trimmedLine.find_first_not_of(" \t");
+        if (start != std::string::npos) {
+            trimmedLine = trimmedLine.substr(start);
         } else {
-            if (line.find("/*") != std::string::npos) {
-                inComment = true; isComment = true;
-            } else if (line.find("//") != std::string::npos) {
+            trimmedLine = "";
+        }
+        
+        // Если внутри блочного комментария
+        if (inBlockComment) {
+            isComment = true;
+            if (trimmedLine.find("*/") != std::string::npos) {
+                inBlockComment = false;
+                // Проверяем, есть ли код после */
+                size_t codePos = trimmedLine.find("*/") + 2;
+                std::string afterBlock = trimmedLine.substr(codePos);
+                size_t commentPos = afterBlock.find("//");
+                if (commentPos != std::string::npos) {
+                    afterBlock = afterBlock.substr(0, commentPos);
+                }
+                // Удаляем пробелы
+                afterBlock.erase(0, afterBlock.find_first_not_of(" \t"));
+                afterBlock.erase(afterBlock.find_last_not_of(" \t") + 1);
+                if (!afterBlock.empty()) {
+                    hasCode = true;
+                    isComment = false;
+                }
+            }
+        } else {
+            // Проверяем на строковый комментарий
+            size_t commentPos = trimmedLine.find("//");
+            if (commentPos != std::string::npos) {
+                // Есть комментарий, проверяем есть ли код до него
+                std::string codePart = trimmedLine.substr(0, commentPos);
+                codePart.erase(0, codePart.find_first_not_of(" \t"));
+                codePart.erase(codePart.find_last_not_of(" \t") + 1);
+                if (!codePart.empty()) {
+                    hasCode = true;  // Есть код до комментария
+                } else {
+                    isComment = true;  // Только комментарий
+                }
+            }
+            // Проверяем на блочный комментарий
+            else if (trimmedLine.find("/*") != std::string::npos) {
                 isComment = true;
+                if (trimmedLine.find("*/") == std::string::npos) {
+                    inBlockComment = true;  // Не закрылся в этой строке
+                }
+            }
+            // Обычная строка (без комментариев)
+            else {
+                if (!trimmedLine.empty()) {
+                    hasCode = true;
+                }
             }
         }
-
-        if (isComment) res.comments++;
-        else if (!isEmpty) res.logical++;
+        
+        // Подсчитываем
+        if (hasCode) {
+            res.logical++;
+        } else if (isComment) {
+            res.comments++;
+        }
     }
+    
+    if (logger) logger->log("Counting finished.");
     return res;
 }
 
-// Фабричный метод создания счетчика
 std::shared_ptr<ICounter> LineCounterCreator::create(std::shared_ptr<ILogger> log) {
     return std::make_shared<LineCounter>(log);
 }
